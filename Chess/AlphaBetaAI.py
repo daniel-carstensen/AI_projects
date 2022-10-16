f4f4fc6aea3a155c4f11083440a771cbaf9eb4b2
Daniel Carstensen <daniel.l.carstensen.24@dartmouth.edu>
2022-10-15 21:50:02 -0400

    created ches project and imported files

---
+++ Chess/AlphaBetaAI.py
@@ -0,0 +1,16 @@
+# Author : Daniel Carstensen
+# Date : 10/xx/2022
+# File name : AlphaBetaAI.py
+# Class : COSC76
+# Purpose :
+
+import chess
+from math import inf
+
+
+class AlphaBetaAI():
+    def __init__(self, depth):
+        pass
+
+    def choose_move(self, board):
+        pass
---
+++ Chess/MinimaxAI.py
@@ -0,0 +1,14 @@
+# Author : Daniel Carstensen
+# Date : 10/xx/2022
+# File name : MinimaxAI.py
+# Class : COSC76
+# Purpose :
+
+import chess
+
+class MinimaxAI():
+    def __init__(self, depth):
+        pass
+
+    def choose_move(self, board):
+        pass
